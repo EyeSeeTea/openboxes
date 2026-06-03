d40d6976167589c1aeb98e0ef0870a77923552fa
Matias Arriola <matias@eyeseetea.com>
2026-06-03 15:22:57 -0300

    Revert "test(custom-roles): cover exact supplemental role checks"
    This reverts commit 919bd5c566d9e4359ca80f6a138101ab46d2c6d5.

--- grails-app/controllers/org/pih/warehouse/RoleInterceptor.groovy
+++ grails-app/controllers/org/pih/warehouse/RoleInterceptor.groovy
@@ -147,7 +147,6 @@ class RoleInterceptor {
 
             Boolean isUserInRole = true
             if (session.user && supplementalRoles.size() > 0) {
-                // Supplemental roles are exact capability flags, not hierarchy roles.
                 isUserInRole = userService.hasAnyRoles(session.user, supplementalRoles)
             }
 
